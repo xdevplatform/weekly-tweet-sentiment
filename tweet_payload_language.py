08b87b9da71756817e97e1922e7379574dedc1e6
Jessica Garson <jgarson@twitter.com>
2020-05-30 21:46:05 -0400

    Update tweet_payload_language.py

--- tweet_payload_language.py
+++ tweet_payload_language.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import requests
 import json
+import requests
 import ast
 import yaml
 
@@ -10,7 +10,9 @@ def create_twitter_url():
     max_results = 100
     mrf = "max_results={}".format(max_results)
     q = "query=from:{}".format(handle)
-    url = "https://api.twitter.com/labs/2/tweets/search?{}&{}".format(mrf, q)
+    url = "https://api.twitter.com/labs/2/tweets/search?tweet.fields=lang&{}&{}".format(
+        mrf, q
+    )
     return url
 
 
@@ -29,12 +31,18 @@ def twitter_auth_and_connect(bearer_token, url):
     return response.json()
 
 
-def no_tweets(res_json):
-    if res_json == {"meta": {"result_count": 0}}:
-        print("The Twitter handle entered hasn't Tweeted in 7 days.")
+def connect_to_azure(data):
+    azure_url = "https://week.cognitiveservices.azure.com/"
+    sentiment_url = "{}text/analytics/v2.1/sentiment".format(azure_url)
+    subscription_key = data["azure"]["subscription_key"]
+    return sentiment_url, subscription_key
 
 
-def lang_data_shape(res_json):
+def azure_header(subscription_key):
+    return {"Ocp-Apim-Subscription-Key": subscription_key}
+
+
+def create_document_format(res_json):
     data_only = res_json["data"]
     doc_start = '"documents": {}'.format(data_only)
     str_json = "{" + doc_start + "}"
@@ -43,46 +51,8 @@ def lang_data_shape(res_json):
     return ast.literal_eval(doc)
 
 
-def connect_to_azure(data):
-    azure_url = "https://week.cognitiveservices.azure.com/"
-    language_api_url = "{}text/analytics/v2.1/languages".format(azure_url)
-    sentiment_url = "{}text/analytics/v2.1/sentiment".format(azure_url)
-    subscription_key = data["azure"]["subscription_key"]
-    return language_api_url, sentiment_url, subscription_key
-
-
-def azure_header(subscription_key):
-    return {"Ocp-Apim-Subscription-Key": subscription_key}
-
-
-def generate_languages(headers, language_api_url, documents):
-    response = requests.post(language_api_url, headers=headers, json=documents)
-    return response.json()
-
-
-def combine_lang_data(documents, with_languages):
-    langs = pd.DataFrame(with_languages["documents"])
-    lang_iso = [x.get("iso6391Name")
-                for d in langs.detectedLanguages if d for x in d]
-    data_only = documents["documents"]
-    tweet_data = pd.DataFrame(data_only)
-    tweet_data.insert(2, "language", lang_iso, True)
-    json_lines = tweet_data.to_json(orient="records")
-    return json_lines
-
-
-def add_document_format(json_lines):
-    docu_format = '"' + "documents" + '"'
-    json_docu_format = "{}:{}".format(docu_format, json_lines)
-    docu_align = "{" + json_docu_format + "}"
-    jd_align = json.dumps(docu_align)
-    jl_align = json.loads(jd_align)
-    return ast.literal_eval(jl_align)
-
-
 def sentiment_scores(headers, sentiment_url, document_format):
-    response = requests.post(
-        sentiment_url, headers=headers, json=document_format)
+    response = requests.post(sentiment_url, headers=headers, json=document_format)
     return response.json()
 
 
@@ -105,13 +75,9 @@ def main():
     data = process_yaml()
     bearer_token = create_bearer_token(data)
     res_json = twitter_auth_and_connect(bearer_token, url)
-    no_tweets(res_json)
-    documents = lang_data_shape(res_json)
-    language_api_url, sentiment_url, subscription_key = connect_to_azure(data)
+    sentiment_url, subscription_key = connect_to_azure(data)
     headers = azure_header(subscription_key)
-    with_languages = generate_languages(headers, language_api_url, documents)
-    json_lines = combine_lang_data(documents, with_languages)
-    document_format = add_document_format(json_lines)
+    document_format = create_document_format(res_json)
     sentiments = sentiment_scores(headers, sentiment_url, document_format)
     week_score = mean_score(sentiments)
     print(week_score)
@@ -120,3 +86,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
